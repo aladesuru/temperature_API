3c5bf7f89e725d7d6c90f21844ccb7380d2b5e8d
adebola <bola@ringley.co.uk>
2018-05-10 07:02:28 +0100

    styling the display to broswer

--- js/main.js
+++ js/main.js
@@ -1,81 +1,90 @@
 // Variable declaration 
 let $url = location.pathname;
-let $tempReading = [];
+let $tempReading = []; 
 let $time = null;
 let $averge = [];
 let $clearAverageInerval = null;
 
+
+
+
 /***************************************************************************/
-/*  Create a Temperature Utilities :
-/ * 1 for login and connect to API
-/*  2 to call API every minute to take latest temperature reading
-/*  3 to temperature reading for client                                        
+/*  TEMPERATURE UTILITIES :
+/*
+/*  1 to check if a user has signed in
+/ * 2 for login and connect to API
+/*  3 to call API every minute to take latest temperature reading
+/*  4 to calculated average and store calculated average
+/*  5 that allow any length of time series and any interval
+/*  6 that display temperature to browser                                        
 /***************************************************************************/
 
 const temperature_utilities = {
-//Method to sniff if a user has  sign in 
+//Method to check if a user has signed in 
  init: () => {
-   if (window.sessionStorage.getItem("token")) {  
-      temperature_utilities.callTemApi();
-       setInterval(() => {
-    //calling the API every minute to take the latest temperature reading
-         temperature_utilities.callTemApi();
+   if (window.sessionStorage.getItem("token")) {              // if signed in display display temperature
 
-    //generate timestamp , store the timestamp and the API generated every minute
-          $tempReading.push({timestamp : Date.now() , temp: Number(sessionStorage.temperature)});
-          // console.log($tempReading); 
+      temperature_utilities.callTemApi('temp.json');
+
+       setInterval(() => {
+    //Calling the API every minute to take the latest temperature reading
+      temperature_utilities.callTemApi('temp.json');
+
+    //Generate timestamp , store the timestamp and the API generated every minute in array
+    // and passing the array to average method to calculate average
+      $tempReading.push({timestamp : Date.now() , temp: Number(sessionStorage.temperature)});
+
        },60000); 
 
-    //generate average to produce a smaler set of data that can serve across our API
-      // $clearAverageInterval= setInterval(() => {
-      //   temperature_utilities.averageTemp($tempReading);
-      //  }, 300000);
-      temperature_utilities.timeSeries(1,5);
+    //Calling code to produce a smaller set of data that can serve across our API
+      temperature_utilities.smallerDataSet(1,5);
+
    } else {
-     $("#loginForm").fadeIn(800);
+      $('#Form-container').css('transform' , 'scale(1)');  // if not signed in display login form
    }
  },
 
 //Method to login and connect to API to return token
-    login: () => {
-       let $loginUrl = `${$url}${'token.json'}`;
-       let $loginDetails = {"user" : $('#user').val(), "password" : $('#password').val() } ;
-       let $formData  = JSON.stringify($loginDetails) ;    
-        $.ajax({
-           url: $loginUrl,
-           type: 'POST',
-           data: $formData ,
-           contentType: 'application/Json',
-           processData: false,
-           dataType: 'json',
-           success: function (response) {
-            if (response.token) {
-               window.sessionStorage.setItem("token", response.token);
-               location.reload();
-             } else {
-               alert('Invalid Login')
-             }
-           },
-           error: function(error){
-             console.info(error);
-           }
-       });         
+    login: (file) => {
+     let $loginUrl = `${$url}${file}`;  
 
-       },
+    let $loginDetails = {"user" : $('#user').val(), "password" : $('#password').val() } ;
+    let $formData  = JSON.stringify($loginDetails) ;    // Convert loging details to JSON
+      $.ajax({
+         url: $loginUrl,
+         type: 'POST',
+         data: $formData ,
+         contentType: 'application/Json',
+         processData: false,
+         dataType: 'json',
+         success: function (response) {
+          if (response.token) {
+             window.sessionStorage.setItem("token", response.token);
+             location.reload();
+           } else {
+             alert('Invalid Login')
+           }
+         },
+         error: function(error){
+           console.info(error);
+         }
+     });         
+    },
 
 //Method to request  temperature reading
-    callTemApi : () => {
-     let $callTemAPiUrl = `${$url}${'temp.json'}`;
+    callTemApi : (file) => {
+     let $callTemAPiUrl = `${$url}${file}`;
    $.ajax({
        url: $callTemAPiUrl,
        type: 'GET',
        headers: {
-           token: window.sessionStorage.getItem("token"),   //with the request containing the bearer token  
+           token: window.sessionStorage.getItem("token"),   //request containing the bearer token  
        },
        dataType: 'json',
        success: function (data) {
          console.info(data); 
-         window.sessionStorage.setItem("temperature", data.temp);
+         sessionStorage.setItem("temperature", data.temp);  //store the temperature on client side
+         temperature_utilities.showTempOnbrowswer();        // Display temperature to browser
        },
        error: function(error){
          console.info(error);
@@ -88,26 +97,24 @@ const temperature_utilities = {
  averageTemp: (datalist) => {        
    let totalTimestamp = 0;
    let totalTemperature = 0;
-   for (i = 0; i < datalist.length; i++) {
+   for (i = 0; i < datalist.length -1 ; i++) {
        totalTimestamp += datalist[i].timestamp ;
        totalTemperature += datalist[i].temp ;
    }
    averageTemp =  totalTemperature / datalist.length;
    averageTimestamp =  totalTimestamp / datalist.length;
-
    $averge.push({timestamp: averageTimestamp , temp: averageTemp});
 
-   // console.log({timestamp: averageTimestamp , temp: averageTemp});
    console.log($averge);
  },
 
 //Method that allow calling code to specify any length of input time
-// and any interval duration, but have default of 24hour.
-  timeSeries: (tempReadingLenghtofTime = 24 , tempReadingInterval= 5) => { 
-    let interval = tempReadingInterval * (1000 * 60);
-    let timeSeries = tempReadingLenghtofTime * (1000 * 60 * 60); 
+// and any interval duration, but have default of 24 and 5 respectively .
+  smallerDataSet: (tempReadingLenghtofTime = 24 , tempReadingInterval= 5) => { 
+    let interval = tempReadingInterval * (1000 * 60);             // convert the interval to millisecond
+    let lenghtofTime = tempReadingLenghtofTime * (1000 * 60 * 60); // convert the length of time to millisecond
 
-      $clearAverageInerval = setInterval(() => {
+      $clearAverageInerval = setInterval(() => {  
         temperature_utilities.averageTemp($tempReading);
       }, interval);
 
@@ -115,15 +122,22 @@ const temperature_utilities = {
         sessionStorage.removeItem("token");
         clearInterval($clearAverageInerval);
         location.reload(true);
-      },timeSeries);
-  }
+      },lenghtofTime);
+  },
+
+// Method that display temperature to the browser
+  showTempOnbrowswer : () => {
+    let  $displayReading = `<div class='temp'><h2>London Office Temperature</h2>
+                            <span>${sessionStorage.temperature}<sup>o</sup></span></div>`;
+    $('.container').html($displayReading);
+  },
 
 }
 
 // temperature_utilities.login();
 temperature_utilities.init();
+
 $('form#login').submit(function(event){
      event.preventDefault();
-     temperature_utilities.login();
-     $("#loginForm").fadeOut();
+     temperature_utilities.login('token.json');
    });
